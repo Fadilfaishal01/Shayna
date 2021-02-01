80c4c8c793d1a64c0a8b3e1b6c237890a76d74df
Fadil Faishal <pudgelovers1st@gmail.com>
2021-02-01 10:52:58 +0700

    Shayna Buildwithangga

---
+++ src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <Header />
+    <HeroSection />
+    <WomanBanner />
+    <Instagram />
+    <PartnerLogo />
+    <Footer />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Header from '@/components/Header.vue'
+import HeroSection from '@/components/HeroSection.vue'
+import WomanBanner from '@/components/WomanBanner.vue'
+import Instagram from '@/components/Instagram.vue'
+import PartnerLogo from '@/components/PartnerLogo.vue'
+import Footer from '@/components/Footer.vue'
+
+
+export default {
+  name: 'Home',
+  components: {
+    Header,
+    HeroSection,
+    WomanBanner,
+    Instagram,
+    PartnerLogo,
+    Footer
+  }
+}
+</script>
